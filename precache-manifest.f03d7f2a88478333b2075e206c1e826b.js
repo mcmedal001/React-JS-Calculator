e781d75cb732cad1eede99a125f1ae2c58cd176d
Ahmed Ogundimu <ogundimu.ahmed@gmail.com>
2020-04-25 19:58:18 +0100

    Updates

---
+++ precache-manifest.f03d7f2a88478333b2075e206c1e826b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3279d3f1cf5e90ad0a0cc78a288a3ad",
+    "url": "/React-JS-Calculator/index.html"
+  },
+  {
+    "revision": "7f82a91f8b427cee0873",
+    "url": "/React-JS-Calculator/static/css/main.6ccd17cc.chunk.css"
+  },
+  {
+    "revision": "b3624b394ce3188118e2",
+    "url": "/React-JS-Calculator/static/js/2.4caff9a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-JS-Calculator/static/js/2.4caff9a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f82a91f8b427cee0873",
+    "url": "/React-JS-Calculator/static/js/main.02b5779c.chunk.js"
+  },
+  {
+    "revision": "a400a7ad970bb8f6069a",
+    "url": "/React-JS-Calculator/static/js/runtime-main.5dba8f5c.js"
+  }
+]);
